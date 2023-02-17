dc6fd779824da8af174d04ee9a361e203df58e49
OlgaMykhailova <rosebud_z@yahoo.com>
2023-02-17 16:54:01 +0200

    added register, login forms without styles, redux auth draft version

---
+++ src/components/RegisterForm/RegisterForm.jsx
@@ -0,0 +1,71 @@
+import { useState } from 'react';
+import { Formik, Form } from 'formik';
+import { Link } from 'react-router-dom';
+import { useDispatch } from 'react-redux';
+
+import { StepSwitcher } from './StepSwitcher';
+import { registerSchema } from '../../schemas/authValidationSchemas';
+import { register, login } from 'redux/auth/authOperations';
+
+export const RegisterForm = () => {
+  const [page, setPage] = useState(0);
+
+  const dispatch = useDispatch();
+
+  const initialValues = {
+    email: '',
+    password: '',
+    confirmPassword: '',
+    name: '',
+    location: '',
+    phone: '',
+  };
+
+  const handleSubmit = (values, { resetForm }) => {
+    const newUser = {
+      email: values.email,
+      password: values.password,
+      name: values.name,
+      location: values.location,
+      phone: values.phone,
+    };
+    console.log(newUser);
+    const data = dispatch(register(newUser));
+    if (data.type === 'auth/register/fulfilled') {
+      dispatch(login({ email: values.email, password: values.password }));
+      resetForm();
+      setPage(0);
+    }
+    if (!data.payload) {
+      console.log('Something wrong, please try again later');
+    }
+  };
+
+  const handleClick = () => {
+    page === 0 ? setPage(page + 1) : setPage(page - 1);
+  };
+
+  return (
+    <div>
+      <h2>Registration</h2>
+      <Formik
+        initialValues={initialValues}
+        validationSchema={registerSchema}
+        onSubmit={handleSubmit}
+      >
+        <Form>
+          {<StepSwitcher page={page} />}
+          <div>
+            <button type="button" onClick={handleClick}>
+              {page === 0 ? 'Next' : 'Back'}
+            </button>
+            {page === 1 && <button type="submit">Register</button>}
+            <p>
+              Already have an account? <Link to="/login">Login</Link>
+            </p>
+          </div>
+        </Form>
+      </Formik>
+    </div>
+  );
+};
--- src/pages/RegisterPage/RegisterPage.jsx
+++ src/pages/RegisterPage/RegisterPage.jsx
@@ -1,5 +1,11 @@
+import { RegisterForm } from 'components/RegisterForm/RegisterForm';
+
 const RegisterPage = () => {
-  return <div>RegisterPage</div>;
+  return (
+    <div>
+      <RegisterForm />
+    </div>
+  );
 };
 
 export default RegisterPage;
