1c11d28cc384d12728da6502816f6c229506ed26
Artem Slatin <101586284+artemslt@users.noreply.github.com>
2023-02-17 13:16:00 +0200

    Update App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -32,16 +32,16 @@ export const App = () => {
   );
 };
 
-const Wrapper = styled.div`
-  font-family: ${prop => prop.theme.fonts[1]};
+// const Wrapper = styled.div`
+//   font-family: ${prop => prop.theme.fonts[1]};
 
-  ${up('mobile')} {
-    background: ${prop => prop.theme.colors.activeAccent};
-  }
-  ${up('tablet')} {
-    background: ${prop => prop.theme.colors.background};
-  }
-  ${up('pc')} {
-    background: ${prop => prop.theme.colors.gradient};
-  }
-`;
+//   ${up('mobile')} {
+//     background: ${prop => prop.theme.colors.activeAccent};
+//   }
+//   ${up('tablet')} {
+//     background: ${prop => prop.theme.colors.background};
+//   }
+//   ${up('pc')} {
+//     background: ${prop => prop.theme.colors.gradient};
+//   }
+// `;
