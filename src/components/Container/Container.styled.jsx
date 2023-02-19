598a7847108cec0247e8e474fa8c8603ecb80596
Oleksii Shevchenko <vazerius@gmail.com>
2023-02-19 23:06:41 +0200

    контейнер фікс
    зробив на контейнер три брекпоінта, переніс його в компоненти, колір лишив як був на боді, в стилях лайаута поки за комітив, потім вже вирішимо що і як будемо робити.

--- src/components/Container/Container.styled.jsx
+++ src/components/Container/Container.styled.jsx
@@ -4,29 +4,15 @@ import { up } from 'styled-breakpoints';
 export const Container = styled.div`
   margin: auto;
   padding: 0 20px;
-  ${up('mobileSizeM')} {
-    max-width: 640px;
-    padding: 0 20px;
-  }
+  max-width: 640px;
+  padding: 0 20px;
+
   ${up('tablet')} {
     max-width: 768px;
     padding: 0 32px;
   }
-  ${up('tabletM')} {
-    max-width: 820px;
-    padding: 0 32px;
-  }
 
   ${up('pcSizeS')} {
-    max-width: 960px;
-    padding: 0 32px;
-  }
-
-  ${up('pcSizeM')} {
-    max-width: 1199px;
-    padding: 0 32px;
-  }
-  ${up('pcSizeL')} {
     max-width: 1280px;
     padding: 0 16px;
   }
