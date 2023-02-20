d6088fcd38af9a4ce59510f080efa9f8bdf8a504
Oleksii Shevchenko <vazerius@gmail.com>
2023-02-20 23:00:52 +0200

    прибрав коміт
    прибрав коміт

--- src/components/Modal/Modal.jsx
+++ src/components/Modal/Modal.jsx
@@ -1,47 +1,5 @@
-// import React, { useEffect } from 'react';
-// import { createPortal } from 'react-dom';
-// // import css from './modal.module.css';
-// import { Overlay, Modal } from './Modal.styled';
-
-// const modalRoot = document.querySelector('#modal-root');
-
-// export const ModalMenu = ({ children, open, onClose }) => {
-//   useEffect(() => {
-//     window.addEventListener('keydown', handlerKeyDown);
-//     function handlerKeyDown(e) {
-//       if (e.code === 'Escape') {
-//         onClose();
-//       }
-//     }
-
-//     return () => {
-//       window.removeEventListener('keydown', handlerKeyDown);
-//     };
-//   }, [onClose]);
-
-//   function onCloseOverlay(e) {
-//     if (e.target === e.currentTarget) {
-//       onClose();
-//     }
-//   }
-
-//   return createPortal(
-//     <Overlay
-//       onKeyDown={onCloseOverlay}
-//       onClick={e => onCloseOverlay(e)}
-//       className={!open ? 'isHidden' : 'show'}
-//     >
-//       <Modal className={open ? 'show ' : null}>
-//         {open ? <>{children}</> : null}
-//       </Modal>
-//     </Overlay>,
-//     modalRoot
-//   );
-// };
-
 import React, { useEffect } from 'react';
 
-// import css from './modal.module.css';
 import { Overlay, Modal } from './Modal.styled';
 
 export const ModalMenu = ({ children, open, onClose }) => {
