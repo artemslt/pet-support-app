af1032c5c84ce3cd46e9e7fa9401411b0f310893
orlyanSKY <000slim001@gmail.com>
2023-02-21 17:30:16 +0300

    notices

--- src/components/AddNoticeButton/AddNoticeButton.jsx
+++ src/components/AddNoticeButton/AddNoticeButton.jsx
@@ -4,7 +4,7 @@ import { ReactComponent as PlusIcon } from './akar-icons_plus.svg';
 export const AddNoticeButton = () => {
   return (
     <AddButton>
-      Add pet{' '}
+      Add pet
       {
         <Circle>
           <PlusIcon />
