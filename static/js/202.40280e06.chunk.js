4f6663f58bb6ab02699b9df33ccda05ad2f0d1da
artemslt <101586284+artemslt@users.noreply.github.com>
2023-02-21 16:59:08 +0000

    Deploying to gh-pages from @ artemslt/pet-support-app@956eaaadc8d161ed6d3adfed44e2ea8657981dbc 🚀

---
+++ static/js/202.40280e06.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkpet_support_app=self.webpackChunkpet_support_app||[]).push([[202],{9202:function(p,e,t){t.r(e);var u=t(184);e.default=function(){return(0,u.jsx)("div",{children:"NoticesPage"})}}}]);
+//# sourceMappingURL=202.40280e06.chunk.js.map
