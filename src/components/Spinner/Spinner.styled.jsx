97993aa48cc70a28b44a73e6cba6be98bc1a11e9
Nazar Lyubuska <nazar.lyubuska@gmail.com>
2023-02-21 15:25:16 +0200

    create NewsPage

---
+++ src/components/Spinner/Spinner.styled.jsx
@@ -0,0 +1,22 @@
+import styled from "styled-components";
+
+export const Spinner = styled.div`
+  margin-top: 60px;
+  margin-bottom: 100px;
+  border: 16px solid peachpuff;
+  border-top: 16px orange solid;
+  border-radius: 50%;
+  height: 120px;
+  width: 120px;
+  animation: spin 2s linear infinite;
+
+  @keyframes spin {
+    0% {
+      transform: rotate(0deg);
+    }
+
+    100% {
+      transform: rotate(360deg);
+    }
+  }
+`;
