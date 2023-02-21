1abd42c149f516a38c14b2349d4a2d5df6d1ba07
Xdecent <xdecentwork@gmail.com>
2023-02-21 21:42:39 +0200

    some new styles. but not finished

--- src/components/Friend/Friend.styled.jsx
+++ src/components/Friend/Friend.styled.jsx
@@ -1,90 +1,54 @@
 import styled from 'styled-components';
 import { theme } from '../../theme';
-const { space, fontSizes, fontWeights, colors } = theme;
-
-export const PartnerTitle = styled.h1`
-  font-weight: ${fontWeights[2]};
-  text-align: center;
-  color: ${colors.additionalBlack};
-  font-size: ${fontSizes[6]}px;
-  line-height: ${space[5]}px;
-
-  @media (max-width: ${theme.mobile}) {
-    font-size: ${fontSizes[1]}px;
-    line-height: ${space[2]}px;
-    margin: ${space[5]}px 0 ${space[4]}px;
-  }
-
-  @media (min-width: ${theme.mobileSizeM}) {
-    margin: ${space[4]}px 0 ${space[2]}px;
-  }
-
-  @media (min-width: ${theme.pc}) {
-    margin: ${space[3]}px 0;
-  }
-`;
-
-export const CardList = styled.ul`
-  display: grid;
-  margin: 0 auto;
-
-  @media screen and (min-width: ${theme.mobile}) {
-    grid-template-columns: repeat(1, 1fr);
-    max-width: 280px;
-    gap: ${space[1]}px;
-  }
-
-  @media screen and (min-width: ${theme.tablet}) {
-    grid-template-columns: repeat(2, 1fr);
-    max-width: ${theme.tabletM};
-  }
-
-  @media screen and (min-width: ${theme.pc}) {
-    grid-template-columns: repeat(3, 1fr);
-    gap: ${space[4]}px;
-    max-width: ${theme.pcSizeL};
-  }
-`;
+const { space, fonts, fontSizes, fontWeights, colors } = theme;
 
 export const Card = styled.li`
+font-family: ${fonts[1]};
   display: flex;
   background-color: ${colors.mainWhite};
-  box-shadow: ${space[2]}px ${space[1]}px ${space[4]}px rgba(49, 21, 4, 0.07);
-  border-radius: ${space[5]}px;
+  box-shadow: 7px 4px 14px rgba(49, 21, 4, 0.07);
+  border-radius: 40px;
   padding: ${space[3]}px 0;
   flex-direction: column;
+
+  @media (max-width: 320px) {
+    border-radius: 20px;
+  }
 `;
 
 export const CardTitle = styled.a`
-  font-weight: ${fontWeights[1]};
+  font-weight: ${fontWeights[2]};
   text-decoration-line: underline;
   color: ${colors.mainAccent};
   text-align: center;
-  margin-bottom: ${space[3]}px;
+  max-width: 130px;
+  margin-right: auto;
+  margin-left: auto;
 
-  @media (max-width: ${theme.mobile}) {
-    margin-bottom: ${space[1]}px;
-    font-size: ${fontSizes[0]}px;
-    line-height: ${space[1]}px;
-  }
+  margin-bottom: ${fontSizes[1]}px;
+  font-size: ${fontSizes[0]}px;
+  line-height: 16px;
 
-  @media (min-width: ${theme.mobile}) {
+
+  @media (min-width: 320px) {
+    margin-bottom: ${space[3]}px;
     font-size: ${fontSizes[2]}px;
-    line-height: ${space[3]}px;
+    line-height: 22px;
+    max-width: 250px;
   }
 
-  @media (min-width: ${theme.pc}) {
+  @media (min-width: 1280px) {
     margin-bottom: ${space[3]}px;
     font-size: ${fontSizes[4]}px;
-    line-height: ${space[4]}px;
+    line-height: 27px;
   }
 `;
 
 export const CardInfo = styled.div`
   display: flex;
   flex-direction: row;
-  @media (max-width: ${theme.mobile}) {
-    flex-direction: row;
+  @media (min-width: 1280px) {
+    justify-content: space-evenly;
   }
 `;
 
