99bbde19c935b5592d8950b18e27566515e4fe13
Xdecent <xdecentwork@gmail.com>
2023-02-22 17:03:56 +0200

    new styles + img + not found

--- src/components/Friend/Friend.jsx
+++ src/components/Friend/Friend.jsx
@@ -7,58 +7,66 @@ import {
   CardDataItem,
 } from './Friend.styled';
 
+import notfound from './notfound.jpg';
 
-export const Friend = ({ _id, title,url , imageUrl, addressUrl, address, email, phone, }) => {
+export const Friend = ({ _id, title, url, imageUrl, addressUrl, address, email, phone, workDays }) => {
+    const today = new Date().getDay();
+    const hours = workDays?.[today]?.isOpen ? `${workDays[today].from} - ${workDays[today].to}` : null;
     return (
-            <Card key={_id}>
-            <CardTitle href={url} target="_blank" rel="noreferrer">{title}
-            </CardTitle>
-                <CardInfo>
-                <CardPhoto>
-                    {/* поки що спрвжню заглушку не поставив,а обрав з логотипів  */}
-                        <img src={imageUrl || 'https://storage.googleapis.com/kidslikev2_bucket/pets-support/images/sponsors/frame_288.png'} alt="partner logo" />
-                    </CardPhoto>
-                    <CardData>
-                        <ul>
-                            <CardDataItem>
-                                <span>Time:</span>
-                                <span>-----------------------------</span>
-                            </CardDataItem>
-                            <CardDataItem>
-                                <span>Address:</span>
-                                {address ? (
-                                    <span>
-                                        <a href={addressUrl ?? '#'} target="_blank" rel="noreferrer">
-                                            {address}
-                                        </a>
-                                    </span>
-                                ) : (
-                                    <span>-----------------------------</span>
-                                )}
-                            </CardDataItem>
-                            <CardDataItem>
-                                <span>Email:</span>
-                                {email ? (
-                                    <span>
-                                        <a href={`mailto:${email}`}>{email}</a>
-                                    </span>
-                                ) : (
-                                    <span>-----------------------------</span>
-                                )}
-                            </CardDataItem>
-                            <CardDataItem>
-                                <span>Phone:</span>
-                                {phone ? (
-                                    <span>
-                                        <a href={`tel:${phone}`}>{phone}</a>
-                                    </span>
-                                ) : (
-                                    <span>-----------------------------</span>
-                                )}
-                            </CardDataItem>
-                        </ul>
-                    </CardData>
-                </CardInfo>
-            </Card>
+      <Card key={_id}>
+        <CardTitle href={url} target="_blank" rel="noreferrer">{title}</CardTitle>
+        <CardInfo>
+          <CardPhoto>
+            <img src={imageUrl || notfound} alt="partner logo" />
+          </CardPhoto>
+          <CardData>
+            <ul>
+              <CardDataItem>
+                <span>Time:</span>
+                <div>
+      {hours ? (
+        <span>{hours}</span>
+      ) : (
+        <span>-----------------------------</span>
+      )}
+    </div>
+              </CardDataItem>
+              <CardDataItem>
+                <span>Address:</span>
+                {address ? (
+                  <span>
+                    <a href={addressUrl ?? '#'} target="_blank" rel="noreferrer">
+                      {address}
+                    </a>
+                  </span>
+                ) : (
+                  <span>-----------------------------</span>
+                )}
+              </CardDataItem>
+              <CardDataItem>
+                <span>Email:</span>
+                {email ? (
+                  <span>
+                    <a href={`mailto:${email}`}>{email}</a>
+                  </span>
+                ) : (
+                  <span>-----------------------------</span>
+                )}
+              </CardDataItem>
+              <CardDataItem>
+                <span>Phone:</span>
+                {phone ? (
+                  <span>
+                    <a href={`tel:${phone}`}>{phone}</a>
+                  </span>
+                ) : (
+                  <span>-----------------------------</span>
+                )}
+              </CardDataItem>
+            </ul>
+          </CardData>
+        </CardInfo>
+      </Card>
     );
-}
+  };
+  
