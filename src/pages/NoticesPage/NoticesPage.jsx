1741c89979626e73fb5dd135d253d5ad229d264e
artemslt <ppt.artem@gmail.com>
2023-02-24 16:11:10 +0200

    fix

--- src/pages/NoticesPage/NoticesPage.jsx
+++ src/pages/NoticesPage/NoticesPage.jsx
@@ -8,7 +8,7 @@ import { Outlet } from 'react-router-dom';
 import { AddPet } from '../../components/ModalAddNotice/ModalAddNotice';
 import { ModalMenu } from 'components/Modal/Modal';
 import { ModalMenu } from 'components/Modal/Modal';
-import { useState } from 'react';
+// import { useState } from 'react';
 
 const NoticesPage = () => {
   const [modalToggle, setModalToggle] = useState(false);
