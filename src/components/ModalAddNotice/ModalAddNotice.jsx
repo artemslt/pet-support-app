ec0bb83d3f351b73a7d65225ca66d8667b2ced93
Olena Posternak <alena.kavyrshyna@gmail.com>
2023-02-24 16:19:39 +0200

    Update ModalAddNotice.jsx

--- src/components/ModalAddNotice/ModalAddNotice.jsx
+++ src/components/ModalAddNotice/ModalAddNotice.jsx
@@ -33,9 +33,9 @@ export const AddPet = ({ onToggleModal }) => {
   };
 
   const [step, setStep] = useState(true);
-
-  // стейт для збереження юрл
   const [imgUrl, setImgUrl] = useState(null);
+  const [file, setFile] = useState(null);
+  console.log(file)
 
   // діспатч перемикає перемикач який в редаксі,
   //він потрібен для того щоб в нас вмикалася кнопка на перемикання інпута, після закриття
@@ -53,6 +53,17 @@ export const AddPet = ({ onToggleModal }) => {
 
     if (event.target.files && event.target.files.length > 0) {
       const file = event.target.files[0];
+      const size = file.size;
+      console.log(file)
+      console.log(size)
+      
+      if (Number(size) > 3000000) {
+        console.log(`ERROR`);
+        return;
+        // return toast.error(`Photo must be no larger than 2.8 megabytes`);
+      }
+
+      setFile(file);
       return fileReader.readAsDataURL(file);
     }
     return;
