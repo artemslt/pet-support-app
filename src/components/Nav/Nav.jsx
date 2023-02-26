4cdd3ea5d4f605f8bf006dc93dd48038a4a8bdb6
artemslt <ppt.artem@gmail.com>
2023-02-26 16:32:49 +0200

    fix

--- src/components/Nav/Nav.jsx
+++ src/components/Nav/Nav.jsx
@@ -19,32 +19,15 @@ export const Nav = ({ setOpen }) => {
     <NavContainer>
       <Link to="/news" onClick={() => setOpen(false)}>
         {t('News')}
-        {/* News */}
       </Link>
 
       <Link to="/notices" onClick={() => setOpen(false)}>
         {t('Find_pet')}
-        {/* Find pet */}
       </Link>
 
       <Link to="/friends" onClick={() => setOpen(false)}>
         {t('Our_friends')}
-        {/* Our friends */}
       </Link>
-      <div>
-        {Object.keys(lngs).map(lng => (
-          <button
-            key={lng}
-            style={{
-              fontWeight: i18n.resolvedLanguage === lng ? 'bold' : 'normal',
-            }}
-            type="submit"
-            onClick={() => i18n.changeLanguage(lng)}
-          >
-            {lngs[lng].nativeName}
-          </button>
-        ))}
-      </div>
     </NavContainer>
   );
 };
