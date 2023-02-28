b17e2645e03934fcd329ae454cb8a9a4a85e8456
artemslt <ppt.artem@gmail.com>
2023-02-28 15:00:45 +0200

    fix spinner

--- src/components/BurgerMenu/BurgerMenu.styled.jsx
+++ src/components/BurgerMenu/BurgerMenu.styled.jsx
@@ -8,21 +8,15 @@ export const StyledMenu = styled.nav`
   display: flex;
   flex-direction: column;
   z-index: 5;
-  /* height: 100%; */
 
   height: ${({ open }) => (open ? '100%' : '0px')};
   transition-delay: ${({ open }) => (open ? '0' : '0.5s')};
   transition: 0.5s;
-  /* transition-delay: 0.5s; */
   overflow: hidden;
 
   background: ${({ theme }) => theme.colors.background};
-  /* height: 100vh; */
   width: 100vw;
   padding: ${({ open }) => (open ? '16px 20px' : '0')};
-  /* position: absolute; */
-  /* top: 0; */
-  /* left: 0; */
 
   ${up('tablet')} {
     padding: ${({ open }) => (open ? '24px 32px' : '0')};
